dba5c03152579e10b771b397295d3be1a137e7e3
Emilio <emiliovillanuevaosorio3@gmail.com>
2025-10-01 20:35:01 -0500

    first commit

---
+++ src/main/java/com/example/MarketPost/MarketPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarketPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MarketPost/MarketPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarketPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
